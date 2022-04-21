de1c61ec53a359412bde33c5d0b50c652ab7eeda
ccppoo <shmishmi79@gmail.com>
2022-04-21 19:42:59 +0900

    Update message.py

--- kakaotalk/message.py
+++ kakaotalk/message.py
@@ -15,3 +15,14 @@ class KTMessage:
     def concat(self, string : str) -> KTMessage:
         self.message += string
         return self
+
+    def __hash__(self) -> int:
+        return hash((self.sender, self.time, self.message))
+
+    def __eq__(self, other: KTMessage) -> bool:
+        if not isinstance(other, KTMessage):
+            return False
+
+        return self.sender == other.sender and \
+            self.time == other.time and \
+            self.message == other.message
