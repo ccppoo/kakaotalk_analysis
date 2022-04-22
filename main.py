d77e556cb43fb5f5f68f944348c7c774d21cf28e
ccppoo <shmishmi79@gmail.com>
2022-04-22 19:41:28 +0900

    csv 제작, db 파일 제작 스크립트 작성중

--- main.py
+++ main.py
@@ -8,6 +8,25 @@ def main():
     with open(KakaoTalk_TXT, encoding='utf-8', mode='r') as fp:
         talks = kakaotalk.Talks(fp=fp)
 
+    print(len(talks.messages))
+
+    import csv
+
+    with open(csv_temp, newline='', encoding='utf-8', mode='w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['Type', 'Sender', 'TIME' ,'message'])
+
+        format_FULL= "%Y-%m-%d %H:%M"
+        format_YMD = "%Y-%m-%d"
+        format_HM = "%H:%M"
+        
+        for m in talks.messages:
+            writer.writerow([
+                m.type,
+                m.sender.name, 
+                m.time.strftime(format_FULL),
+                m.message
+            ])
 
 if __name__ == "__main__":
     main()
