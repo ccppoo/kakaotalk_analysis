8e17c4458a82bde23d9ffc71f142abf0393b2e86
ccppoo <shmishmi79@gmail.com>
2022-05-06 16:25:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from datetime import datetime
 from os import PathLike
 from kakaotalk import *
 import kakaotalk
 import csv
 
-KakaoTalk_TXT = "<PATH TO KakaoTalk txt file>"
+KakaoTalk_TXT = "<Path to kakaotalk txt file>"
 
 all_event_parsed = "kakaotalk_events.csv"
 Join_Out_Kick_event_parsed = "join_out_kick_events.csv"
 message_pasred = "talk_parsed.csv"
+filter_date = 'date_filtered_messages.csv'
 
 def loadFile(filename : PathLike) -> Talks:
     with open(filename, encoding='utf-8', mode='r') as fp:
@@ -62,7 +64,27 @@ def makeCSV_all_messages():
                 m.message
             ])        
 
+def filter_messages_by_date():
+    talks = loadFile(KakaoTalk_TXT)
+
+    start = datetime(2022, 3, 1)
+    end = datetime(2022, 3, 31)
+    myFilter = lambda x : x._time >= start and x._time <= end
+
+    with open(filter_date, newline='', encoding='utf-8', mode='w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['Type', 'Sender', 'Time' ,'Message'])
+        
+        for m in filter(myFilter, talks.messages):
+            writer.writerow([
+                m.message_type, 
+                m.sender.name,
+                m.time,
+                m.message
+            ])
+
 if __name__ == "__main__":
     makeCSV_all_event()
     makeCSV_all_messages()
     makeCSV_Join_Out_Kick_event()
+    filter_messages_by_date()
